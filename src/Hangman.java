28c071b667672c3cb1f2812dd93f1535ff0c9bd2
MichaelMarcuzziSCC <W0878029@myscc.ca>
2025-09-25 15:45:24 -0400

    Integrated File I/O
    Revised the menu to use txt files and created a method loadWordsFromFile to read the files and create an array with the words

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,7 +1,8 @@
 // MAD 300 Lab 1-3
 // Author: Michael Marcuzzi
 // Date: 2025-09-25
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 /*
@@ -150,3 +151,103 @@ public class Hangman {
 }*/
 
 // Lab 3
+public class Hangman {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean cont = true;
+
+        while (cont) {
+            System.out.println("\n===== Hangman Menu =====");
+            System.out.println("1. Countries");
+            System.out.println("2. Food");
+            System.out.println("3. Animals");
+            System.out.println("4. Exit");
+            System.out.print("Choose a topic (1-4): ");
+
+            int choice = input.nextInt();
+            input.nextLine(); // consume newline
+
+            String filename;
+
+            switch (choice) {
+                case 1:
+                    filename = "countries.txt";
+                    break;
+                case 2:
+                    filename = ".idea/food.txt";
+                    break;
+                case 3:
+                    filename = "animals.txt";
+                    break;
+                case 4:
+                    System.out.println("Exiting game. Goodbye!");
+                    cont = false;
+                    continue;
+                default:
+                    System.out.println("Invalid choice, try again.");
+                    continue;
+            }
+
+            // Load words from the selected file
+            List<String> words = loadWordsFromFile(filename);
+
+            if (words.isEmpty()) {
+                System.out.println("No words found in " + filename + ". Please check the file.");
+                continue;
+            }
+
+            playHangman(words, input);
+        }
+
+        input.close();
+    }
+
+    // Method to read words from a file
+    public static List<String> loadWordsFromFile(String filename) {
+        List<String> words = new ArrayList<>();
+        try (Scanner fileScanner = new Scanner(new File(filename))) {
+            while (fileScanner.hasNextLine()) {
+                String word = fileScanner.nextLine().trim().toLowerCase();
+                if (!word.isEmpty()) {
+                    words.add(word);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found -> " + filename);
+        }
+        return words;
+    }
+
+    public static void playHangman(List<String> words, Scanner input) {
+        // Pick a random word and create a new array with equal length
+        String word = words.get((int) (Math.random() * words.size()));
+        char[] guessedWord = new char[word.length()];
+        // Fill with *'s
+        for (int i=0; i<guessedWord.length; i++) {
+            guessedWord[i] = '*';
+        }
+
+        // While the guessed word does not equal the actual word..
+        int misses = 0;
+        while (!String.valueOf(guessedWord).equals(word)) {
+            System.out.print("(Guess) Enter a letter in word " + String.valueOf(guessedWord) + " > ");
+            char guess = input.next().charAt(0);
+
+            // If letter already guessed
+            if (String.valueOf(guessedWord).indexOf(guess) >= 0) {
+                System.out.println(guess + " is already in the word");
+            } else if (word.indexOf(guess) < 0) { // Wrong guess
+                System.out.println(guess + " is not in the word");
+                misses++;
+            } else { // Correct guess
+                for (int i=0; i<word.length(); i++) {
+                    if (word.charAt(i) == guess) {
+                        guessedWord[i] = guess;
+                    }
+                }
+            }
+        }
+        // Word completed
+        System.out.println("The word is " + word + ". You missed " + misses + (misses == 1 ? " time" : " times"));
+    }
+}
