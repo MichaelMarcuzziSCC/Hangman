8023116738e59153b4dfc6c52005f7ee162d3f2a
MichaelMarcuzziSCC <W0878029@myscc.ca>
2025-09-25 15:13:11 -0400

    Variable Initialization
    initializing variables to be used

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,8 +2,13 @@
 // Author: Michael Marcuzzi
 // Date: 2025-09-25
 
+import java.util.*;
+
 public class Hangman {
     public static void main(String[] args) {
-
+        // Variable init
+        Scanner input = new Scanner(System.in);
+        String[] words = {"write", "that", "program", "java", "computer", "hangman"};
+        boolean cont = true;
     }
 }
